25cd448900a5935201f6878f21011146874f7e64
Porter Webster <porterwwebster@gmail.com>
2018-08-18 16:11:28 -0600

    modified a few tag elements that needed fixing. Added onclick function to buttons. Added JavaScript file to pages.

---
+++ shared.js
@@ -0,0 +1,15 @@
+const showOne = () => {
+  console.log("Clicked Show 1")
+}
+const showTwo = () => {
+  console.log("Clicked Show 2")
+}
+const createUpdate = () => {
+  console.log("Clicked Create/Update")
+}
+const deleteShowOne = () => {
+  console.log("Deleted Show One from listing")
+}
+const deleteShowTwo = () => {
+  console.log("Deleted Show Two from listing")
+}
